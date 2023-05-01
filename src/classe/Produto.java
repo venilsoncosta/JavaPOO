b96ad8dda07c8275260be36d89a79b9f71d661af
Venilson Neves <38733697+venilsoncosta@users.noreply.github.com>
2023-05-01 12:51:30 -0300

    att

--- src/classe/Produto.java
+++ src/classe/Produto.java
@@ -4,4 +4,12 @@ public class Produto {
     String nome;
     double preco;
     double desconto;
+
+    double precoComDesconto(){
+        return preco * (1 - desconto);
+    }
+    double precoComDescontoGerente(){
+        return preco * (1 - desconto + 0.06);
+    }
 }
+
--- src/classe/ProdutoTeste.java
+++ src/classe/ProdutoTeste.java
@@ -7,5 +7,8 @@ public class ProdutoTeste {
         p1.preco = 2456;
         p1.desconto = 0.25;
         System.out.println(p1.nome);
+        System.out.println(p1.precoComDesconto());
+        double precoFinal = (p1.precoComDesconto() + p1.precoComDescontoGerente()) / 2;
+        System.out.println(precoFinal);
     }
 }
