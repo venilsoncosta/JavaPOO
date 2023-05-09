370eee9f4d984fbe55f15cda52008468bbcf559c
Venilson Neves <38733697+venilsoncosta@users.noreply.github.com>
2023-05-09 15:32:51 -0300

    att

---
+++ src/TestandoCoisas.java
@@ -0,0 +1,11 @@
+public class TestandoCoisas {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = a;
+        a++;
+        b--;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println("a= " + a);
+    }
+}
